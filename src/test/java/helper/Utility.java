7c1bce4668548a84bb5cb18294862caa29a0989d
kristfe <kristfe312@gmail.com>
2024-12-02 20:23:55 +0700

    menambahkan When scenario

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -13,34 +13,45 @@ public class Utility {
 
     public static WebDriver driver;
 
-    public static File getJSONSchemaFile(String JSONFile){
-        return new File("src/test/java/helper/JSONSchemaData/"+JSONFile);
-}
+    public static File getJSONSchemaFile(String JSONFile) {
+        return new File("src/test/java/helper/JSONSchemaData/" + JSONFile);
+    }
 
-public static String generateRandomEmail(){
-       String allowedChars ="abcdefghijklmnopqrstuvwxyz" + "1234567890"+"_-.";
-        String email ="";
+    public static String generateRandomEmail() {
+        String allowedChars = "abcdefghijklmnopqrstuvwxyz" + "1234567890" + "_-.";
+        String email = "";
         String temp = RandomStringUtils.randomAlphabetic(10);// panjang email 10 karakter
-    email = temp+"@testdata.com";
-   return email;
+        email = temp + "@testdata.com";
+        return email;
+    }
+
+    public static void startDriver() {
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
+
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan saat memulai chome" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+/*public static WebDriver getDriver(){
+        return driver;
+}*/
+
+    public static void quitDriver() {
+         if (driver != null) {
+        driver.quit();
+        System.out.println("Driver ditutup.");
+        }
+    }
 }
 
-public static void startDriver(){
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--remote-allow-origins=*");
 
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
-}
-
-public static void quitDriver(){
-       driver.quit();
-       System.out.println("driver quit-console lock");
-}
-
-}
